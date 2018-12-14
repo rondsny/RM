48fb378e8de7c85043b023241787c2ebd088b32e
Ron Ngai <rondsny@gmail.com>
2018-12-14 19:37:09 +0800

    Create MBNetTcpSock.cs

---
+++ MBNetTcpSock.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+
+class MBNetTcpSock : MonoBehaviour
+{
+
+    void Start()
+    {
+        Debug.LogWarning("NetTcpSock Start");
+    }
+
+    void Update()
+    {
+
+    }
+}
